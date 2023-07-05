c3108dc3b2b5b9803dffaf8b631292d3b2dd67ea
wanglicong <w15083127382@163.com>
2023-07-05 21:33:11 +0800

    pre

--- knowledge/Morris/MorrisTraversal.java
+++ knowledge/Morris/MorrisTraversal.java
@@ -1,5 +1,6 @@
 package knowledge.Morris;
 
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import sun.nio.cs.ext.MS874;
 
 /**
@@ -29,7 +30,7 @@ public class MorrisTraversal {
                 while (mostRight.right != null || mostRight.right != cur) {
                     mostRight = mostRight.right;
                 }
-                if (mostRight == null) {
+                if (mostRight.right == null) {
                     mostRight.right = cur;
                     cur = cur.left;
                     continue;
@@ -37,7 +38,34 @@ public class MorrisTraversal {
                     mostRight.right = null;
                 }
             }
-            cur = cur.left;
+            cur = cur.right;
         }
+    }
+    public static void morrisPre(Node head) {
+        if (head == null){
+            return;
+        }
+        Node cur =head;
+        Node mostRight=null;
+        while (cur != null) {
+            mostRight=cur.left;
+            if (mostRight!=null) {
+                while (mostRight.right!=null||mostRight.right!=cur) {
+                    mostRight=mostRight.right;
+                }
+                if (mostRight.right == null) {
+                    System.out.println(cur.value+" ");
+                    mostRight.right=cur;
+                    cur=cur.left;
+                    continue;
+                }else {
+                    mostRight.right=null;
+                }
+             }else {
+                System.out.println(cur.value+" ");
+            }
+            cur=cur.right;
+        }
+        System.out.println();
     }
 }
