fb924499345f4f526a8dfaff8f2369d8061f8c6c
wanglicong <w15083127382@163.com>
2023-07-07 22:17:46 +0800

    in

--- knowledge/Morris/MorrisTraversal.java
+++ knowledge/Morris/MorrisTraversal.java
@@ -1,5 +1,7 @@
 package knowledge.Morris;
 
+import com.sun.deploy.security.MozillaSSLRootCertStore;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import jdk.nashorn.internal.ir.annotations.Ignore;
 import sun.nio.cs.ext.MS874;
 
@@ -67,5 +69,29 @@ public class MorrisTraversal {
             cur=cur.right;
         }
         System.out.println();
+    }
+    public static void morrisIn(Node head) {
+        if (head==null) {
+            return;
+        }
+        Node cur = head;
+        Node mostRight = null;
+        while (cur!=null) {
+            mostRight=cur.left;
+            if (mostRight!=null) {
+                while (mostRight.right!=null||mostRight.right!=cur) {
+                    mostRight=mostRight.right;
+                }
+                if (mostRight.right==null) {
+                    mostRight.right=cur;
+                    cur=cur.left;
+                    continue;
+                }else {
+                    mostRight.right=null;
+                }
+            }
+            System.out.println(cur.value+" ");
+            cur=cur.right;
+        }
     }
 }
