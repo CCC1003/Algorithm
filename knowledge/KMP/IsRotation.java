83eb72c1d57ae6e8c32768c2479c3e158be40359
wanglicong <w15083127382@163.com>
2023-07-08 20:49:13 +0800

    data

--- knowledge/KMP/IsRotation.java
+++ knowledge/KMP/IsRotation.java
@@ -11,58 +11,58 @@ public class IsRotation {
         if (a == null || b == null || a.length() != b.length()) {
             return false;
         }
-        String strA=a+a;
-        String strB=b;
-        return getIndexOf(strA,strB)!=-1;
+        String strA = a + a;
+        String strB = b;
+        return getIndexOf(strA, strB) != -1;
     }
 
     //KMP
-    private static int  getIndexOf(String a, String b) {
-        if (a.length()<b.length()) {
+    private static int getIndexOf(String a, String b) {
+        if (a.length() < b.length()) {
             return -1;
         }
-        char[] strA=a.toCharArray();
-        char[] strB=b.toCharArray();
-        int x=0;
-        int y=0;
-        int[] next=getNextArray(strA);
-        while (x<a.length()&&y<b.length()) {
+        char[] strA = a.toCharArray();
+        char[] strB = b.toCharArray();
+        int x = 0;
+        int y = 0;
+        int[] next = getNextArray(strA);
+        while (x < a.length() && y < b.length()) {
             if (strA[x] == strB[y]) {
                 x++;
                 y++;
             } else if (next[y] == -1) {
                 x++;
-            }else{
-                y=next[y];
+            } else {
+                y = next[y];
             }
         }
-        return y==strB.length?x-y:-1;
+        return y == strB.length ? x - y : -1;
     }
 
     private static int[] getNextArray(char[] str) {
-        if (str.length==0) {
+        if (str.length == 0) {
             return null;
         }
-        int[] next=new int[str.length];
-        next[0]=-1;
-        next[1]=0;
-        int i=2;
+        int[] next = new int[str.length];
+        next[0] = -1;
+        next[1] = 0;
+        int i = 2;
         int cn = 0;
-        while (i<next.length) {
-            if (str[i-1] == next[cn]) {
-                next[i++]=++cn;
-            } else if (cn>0) {
-                cn=next[cn];
-            }else {
+        while (i < next.length) {
+            if (str[i - 1] == next[cn]) {
+                next[i++] = ++cn;
+            } else if (cn > 0) {
+                cn = next[cn];
+            } else {
                 next[i++] = 0;
             }
         }
         return next;
     }
-
+//测试
     public static void main(String[] args) {
-        String a= "123456";
-        String b= "612345";
-        System.out.println(isRotation(a,b));
+        String a = "123456";
+        String b = "612345";
+        System.out.println(isRotation(a, b));
     }
 }
