9c39db874405ce6a824be0d580aa32eea9c706e4
wanglicong <w15083127382@163.com>
2023-08-08 22:28:12 +0800

    morris

--- knowledge/Morris/MinDepth.java
+++ knowledge/Morris/MinDepth.java
@@ -1,5 +1,6 @@
 package knowledge.Morris;
 
+import com.sun.jmx.snmp.internal.SnmpIncomingRequest;
 import knowledge.KMP.TreeEqual;
 
 /**
@@ -7,7 +8,7 @@ import knowledge.KMP.TreeEqual;
  * @create 2023-07-11 20:31
  */
 public class MinDepth {
-    public static class TreeNode{
+    public static class TreeNode {
         public int val;
         public TreeNode left;
         public TreeNode right;
@@ -16,29 +17,75 @@ public class MinDepth {
             this.val = val;
         }
     }
+
     //常规递归方法
     public static int minDepth1(TreeNode head) {
-        if (head==null) {
+        if (head == null) {
             return 0;
         }
         return process(head);
     }
 
     private static int process(TreeNode x) {
-        if (x.left==null&&x.right==null) {
+        if (x.left == null && x.right == null) {
             return 1;
         }
         //左右子树有一个不为空
-        int leftH=Integer.MAX_VALUE;
-        if (x.left!=null) {
-            leftH=process(x.left);
+        int leftH = Integer.MAX_VALUE;
+        if (x.left != null) {
+            leftH = process(x.left);
         }
-        int rightH=Integer.MAX_VALUE;
-        if (x.right!=null) {
-            rightH=process(x.right);
+        int rightH = Integer.MAX_VALUE;
+        if (x.right != null) {
+            rightH = process(x.right);
         }
-        return 1+Math.min(leftH,rightH);
+        return 1 + Math.min(leftH, rightH);
     }
-    //morris遍历
 
+    //morris遍历
+// 下面的方法是morris遍历的解
+    public static int minDepth2(TreeNode head) {
+        if (head == null) {
+            return 0;
+        }
+        TreeNode cur = head;
+        TreeNode mostRight = null;
+        int curLevel = 0;
+        int minHeight = Integer.MAX_VALUE;
+        while (cur != null) {
+            mostRight = cur.left;
+            if (mostRight != null) {
+                int rightBoardSize = 1;
+                while (mostRight.right != null && mostRight.right != cur) {
+                    rightBoardSize++;
+                    mostRight = mostRight.right;
+                }
+                if (mostRight.right == null) { // 第一次到达
+                    curLevel++;
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else { // 第二次到达
+                    if (mostRight.left == null) {
+                        minHeight = Math.min(minHeight, curLevel);
+                    }
+                    curLevel -= rightBoardSize;
+                    mostRight.right = null;
+                }
+            } else { // 只有一次到达
+                curLevel++;
+            }
+            cur = cur.right;
+        }
+        int finalRight = 1;
+        cur = head;
+        while (cur.right != null) {
+            finalRight++;
+            cur = cur.right;
+        }
+        if (cur.left == null && cur.right == null) {
+            minHeight = Math.min(minHeight, finalRight);
+        }
+        return minHeight;
+    }
 }
