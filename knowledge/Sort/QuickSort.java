1be5b6d0a6c02e2ca32f75f776e7919e59905f86
wanglicong <w15083127382@163.com>
2023-10-07 22:11:38 +0800

    quick

--- knowledge/Sort/QuickSort.java
+++ knowledge/Sort/QuickSort.java
@@ -7,19 +7,23 @@ import static knowledge.Sort.NetherlandsFlag.swap;
  * @create 2022-01-18 18:00
  */
 public class QuickSort {
-    public static void quickSort(int[] arr){
-        if (arr==null||arr.length<2){ return; }
-        quickSort(arr,0,arr.length-1);
+    public static void quickSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        quickSort(arr, 0, arr.length - 1);
     }
-    public static void quickSort(int[] arr,int l,int r){
-        if(l<r){
-            swap(arr, l+(int) (Math.random() * (r - l + 1)),r);
-            int [] p=partition(arr,l,r);
-            quickSort(arr,l,p[0]-1);  //递归
-            quickSort(arr,p[1]+1,r);  //递归
+
+    public static void quickSort(int[] arr, int l, int r) {
+        if (l < r) {
+            swap(arr, l + (int) (Math.random() * (r - l + 1)), r);
+            int[] p = partition(arr, l, r);
+            quickSort(arr, l, p[0] - 1);  //递归
+            quickSort(arr, p[1] + 1, r);  //递归
         }
     }
-    public static int[] partition(int[] arr,int l,int r){
+
+    public static int[] partition(int[] arr, int l, int r) {
         int less = l - 1;
         int more = r;
         while (l < more) {
@@ -32,7 +36,7 @@ public class QuickSort {
             }
         }
         swap(arr, more, r);
-        return new int[] { less + 1, more };
+        return new int[]{less + 1, more};
     }
 
 }
