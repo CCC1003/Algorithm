0e13e70c6852771a4da2d86d8433a6057d4a3d30
wanglicong <w15083127382@163.com>
2023-10-10 21:09:23 +0800

    bulle

--- knowledge/Sort/BubbleSort.java
+++ knowledge/Sort/BubbleSort.java
@@ -20,10 +20,9 @@ public class BubbleSort {
         }
     }
     public static void swap(int[] arr,int i,int j){
-        arr[i]=arr[i]^arr[j];
-        arr[j]=arr[i]^arr[j];
-        arr[i]=arr[i]^arr[j];
+        int tmp =arr[i];
+        arr[i]=arr[j];
+        arr[j]=tmp;
     }
-
 
 }
