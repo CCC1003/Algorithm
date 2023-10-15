5a01c8c8b556be1a54beb2cf02a310de1e822274
wanglicong <w15083127382@163.com>
2023-10-15 10:33:18 +0800

    merge

--- knowledge/Sort/MergeSort.java
+++ knowledge/Sort/MergeSort.java
@@ -6,33 +6,39 @@ package knowledge.Sort;
  */
 public class MergeSort {
     //归并排序  压栈 先进后出（FILO）
-    public static void mergeSort(int[] arr){
-        if(arr==null||arr.length<2) { return; }
-        mergeSort(arr,0,arr.length-1);
+    public static void mergeSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        mergeSort(arr, 0, arr.length - 1);
     }
-    public static void mergeSort(int[] arr,int l,int r){
-        if(l==r){ return; }
-        int mid=l+((r-l)>>1);
-        mergeSort(arr,l,mid);
-        mergeSort(arr,mid+1,r);
-        merge(arr,l,mid,r);
+
+    public static void mergeSort(int[] arr, int l, int r) {
+        if (l == r) {
+            return;
+        }
+        int mid = l + ((r - l) >> 1);
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
     }
+
     private static void merge(int[] arr, int l, int m, int r) {
-        int[] help=new int[r-l+1];
-        int i=0;
-        int p1=l;
-        int p2=m+1;
-        while (p1<=m&&p2<=r){
-            help[i++]=arr[p1]<=arr[p2]?arr[p1++]:arr[p2++];
+        int[] help = new int[r - l + 1];
+        int i = 0;
+        int p1 = l;
+        int p2 = m + 1;
+        while (p1 <= m && p2 <= r) {
+            help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
         }
-        while(p1<=m){
-            help[i++]=arr[p1++];
+        while (p1 <= m) {
+            help[i++] = arr[p1++];
         }
-        while (p2<=r){
-            help[i++]=arr[p2++];
+        while (p2 <= r) {
+            help[i++] = arr[p2++];
         }
-        for(i=0;i<help.length;i++){
-            arr[l+i]=help[i];
+        for (i = 0; i < help.length; i++) {
+            arr[l + i] = help[i];
         }
     }
 }
