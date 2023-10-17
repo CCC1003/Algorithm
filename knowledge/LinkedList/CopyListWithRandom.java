4d64a779ea058fe5f5c9ab271b20251fff75823d
wanglicong <w15083127382@163.com>
2023-10-17 10:09:18 +0800

    copy

--- knowledge/LinkedList/CopyListWithRandom.java
+++ knowledge/LinkedList/CopyListWithRandom.java
@@ -1,69 +1,74 @@
 package knowledge.LinkedList;
 
 import java.util.HashMap;
+import java.util.WeakHashMap;
 
 /**
  * @author cong
  * @create 2022-07-30 9:18
  */
 public class CopyListWithRandom {
-    public static class Node{
+    public static class Node {
         private int value;
         public Node next;
         public Node rand;
 
 
-        public Node(int data){this.value=data;}
-    }
-    public static Node copyListWithRand1(Node head){
-        HashMap<Node,Node> map=new HashMap<Node,Node>();
-
-        Node cur=head;
-        while (cur!=null){
-            map.put(cur,new Node(cur.value));
-            cur=cur.next;
+        public Node(int data) {
+            this.value = data;
         }
-        cur=head;
-        while (cur!=null){
-            map.get(cur).next=map.get(cur.next);
-            map.get(cur).rand=map.get(cur.rand);
-            cur=cur.next;
+    }
+
+    public static Node copyListWithRand1(Node head) {
+        HashMap<Node, Node> map = new HashMap<Node, Node>();
+
+        Node cur = head;
+        while (cur != null) {
+            map.put(cur, new Node(cur.value));
+            cur = cur.next;
+        }
+        cur = head;
+        while (cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).rand = map.get(cur.rand);
+            cur = cur.next;
         }
         return map.get(head);
     }
-    public static Node copyListWithRand2(Node head){
-        if (head==null){
+
+    public static Node copyListWithRand2(Node head) {
+        if (head == null) {
             return null;
         }
-        Node cur=head;
-        Node next=null;
+        Node cur = head;
+        Node next = null;
 
-        while (cur!=null){
-            next=cur.next;
-            cur.next=new Node(cur.value);
-            cur.next.next=next;
-            cur=next;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = new Node(cur.value);
+            cur.next.next = next;
+            cur = next;
         }
-        cur=head;
-        Node curCopy=null;
-        while (cur!=null){
-            next=cur.next.next;
-            curCopy=cur.next;
-            curCopy.rand=cur.rand!=null?cur.rand.next:null;
-            cur=next;
+        cur = head;
+        Node curCopy = null;
+        while (cur != null) {
+            next = cur.next.next;
+            curCopy = cur.next;
+            curCopy.rand = cur.rand != null ? cur.rand.next : null;
+            cur = next;
         }
 
-        Node res=head.next;
-        cur=head;
+        Node res = head.next;
+        cur = head;
 
-        while (cur!=null){
-            next=cur.next.next;
-            curCopy=cur.next;
-            cur.next=next;
-            curCopy.next=next!=null?next.next:null;
-            cur=next;
+        while (cur != null) {
+            next = cur.next.next;
+            curCopy = cur.next;
+            cur.next = next;
+            curCopy.next = next != null ? next.next : null;
+            cur = next;
         }
         return res;
-
     }
+
 }
