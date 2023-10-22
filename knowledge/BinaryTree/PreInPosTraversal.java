8061efe53e4d96c3b6fad013620b006c6963589a
wanglicong <w15083127382@163.com>
2023-10-22 21:50:48 +0800

    pip

--- knowledge/BinaryTree/PreInPosTraversal.java
+++ knowledge/BinaryTree/PreInPosTraversal.java
@@ -1,6 +1,8 @@
 package knowledge.BinaryTree;
 
+import com.sun.webkit.event.WCMouseWheelEvent;
 import com.sun.xml.internal.bind.v2.model.core.ID;
+import javafx.scene.SnapshotParameters;
 
 import javax.sound.midi.Soundbank;
 import java.util.Stack;
@@ -10,105 +12,109 @@ import java.util.Stack;
  * @create 2022-08-02 21:42
  */
 public class PreInPosTraversal {
-    public static class Node{
+    public static class Node {
         public int value;
         public Node left;
         public Node right;
 
         public Node(int data) {
-            this.value=value;
+            this.value = data;
         }
     }
+
     //递归方法
     //前序遍历
     public static void preOrderRecur(Node head) {
-        if (head==null){
+        if (head == null) {
             return;
         }
-        System.out.println(head.value+" ");
+        System.out.println(head.value + " ");
         preOrderRecur(head.left);
         preOrderRecur(head.right);
     }
+
     //中序遍历
     public static void inOrderRecur(Node head) {
-        if (head==null){
+        if (head == null) {
             return;
         }
         inOrderRecur(head.left);
-        System.out.println(head.value+" ");
+        System.out.println(head.value + " ");
         inOrderRecur(head.right);
     }
+
     //后序遍历
     public static void posOrderRecur(Node head) {
-        if (head==null) {
+        if (head == null) {
             return;
         }
         posOrderRecur(head.left);
         posOrderRecur(head.right);
-        System.out.println(head.value+" ");
+        System.out.println(head.value + " ");
     }
+
     //非递归方法
     //前序遍历
     public static void preOrderUnRecur(Node head) {
         System.out.println("pre-order: ");
-        if (head!=null){
-            Stack<Node> stack=new Stack<Node>();
+        if (head != null) {
+            Stack<Node> stack = new Stack<Node>();
             stack.add(head);
             while (!stack.isEmpty()) {
-                head=stack.pop();
-                System.out.println(head.value+" ");
-                if (head.right!=null){
+                head = stack.pop();
+                System.out.println(head.value + " ");
+                if (head.right != null) {
                     stack.push(head.right);
                 }
-                if (head.left!=null) {
+                if (head.left != null) {
                     stack.push(head.left);
                 }
             }
         }
         System.out.println();
     }
+
     //中序遍历
-    public static void inOrderUnrecur(Node head){
+    public static void inOrderUnrecur(Node head) {
         System.out.println("in-order: ");
-        if (head!=null) {
-            Stack<Node> stack=new Stack<Node>();
-            while (!stack.isEmpty()||head!=null) {
-                if (head!=null){
+        if (head != null) {
+            Stack<Node> stack = new Stack<Node>();
+            while (!stack.isEmpty() || head != null) {
+                if (head != null) {
                     stack.push(head);
-                    head=head.left;
-                }
-                else{
-                    head=stack.pop();
-                    System.out.println(head.value+" ");
-                    head=head.right;
+                    head = head.left;
+                } else {
+                    head = stack.pop();
+                    System.out.println(head.value + " ");
+                    head = head.right;
                 }
             }
 
         }
         System.out.println();
     }
+
     //后序遍历
-    public static void posOrderUnrecur(Node head){
+    public static void posOrderUnrecur(Node head) {
         System.out.println("pos-order: ");
-        if (head!=null) {
-            Stack<Node> s1=new Stack<Node>();
-            Stack<Node> s2=new Stack<Node>();
+        if (head != null) {
+            Stack<Node> s1 = new Stack<Node>();
+            Stack<Node> s2 = new Stack<Node>();
             s1.push(head);
-            while (!s1.isEmpty()){
-                head=s1.pop();
+            while (!s1.isEmpty()) {
+                head = s1.pop();
                 s2.push(head);
-                if (head.left!=null){
+                if (head.left != null) {
                     s1.push(head.left);
                 }
-                if (head.right!=null){
+                if (head.right != null) {
                     s1.push(head.right);
                 }
             }
-            while (!s2.isEmpty()){
-                System.out.println(s2.pop().value+" ");
+            while (!s2.isEmpty()) {
+                System.out.println(s2.pop().value + " ");
             }
         }
         System.out.println();
     }
-
 }
