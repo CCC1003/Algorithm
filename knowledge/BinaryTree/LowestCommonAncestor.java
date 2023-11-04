4c1f11b60627bc0f6c93c9f2d18bf8c8b5eeb633
wanglicong <w15083127382@163.com>
2023-11-04 17:14:13 +0800

    lca

--- knowledge/BinaryTree/LowestCommonAncestor.java
+++ knowledge/BinaryTree/LowestCommonAncestor.java
@@ -5,7 +5,7 @@ package knowledge.BinaryTree;
  * @create 2022-11-18 21:57
  */
 public class LowestCommonAncestor {
-    public static class Node{
+    public static class Node {
         public int value;
         public Node left;
         public Node right;
@@ -14,15 +14,16 @@ public class LowestCommonAncestor {
             this.value = value;
         }
     }
-    public static Node lowestAncestor(Node head,Node o1,Node o2) {
-        if (head==null||head==o1||head==o2){
+
+    public static Node lowestAncestor(Node head, Node o1, Node o2) {
+        if (head == null || head == o1 || head == o2) {
             return head;
         }
-        Node left=lowestAncestor(head.left,o1,o2);
-        Node right=lowestAncestor(head.right,o1,o2);
-        if (left!=null&&right!=null){
+        Node left = lowestAncestor(head.left, o1, o2);
+        Node right = lowestAncestor(head.right, o1, o2);
+        if (left != null && right != null) {
             return head;
         }
-        return left!=null?left:right;
+        return left != null ? left : right;
     }
 }
